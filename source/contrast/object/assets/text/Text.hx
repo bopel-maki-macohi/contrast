a5b6ff136426356241af3eb7b74f921e1c0192af
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-07 15:50:29 -0500

    a font on my pc that was labbeled Terminal (DOSAPP)

--- source/contrast/object/assets/text/Text.hx
+++ source/contrast/object/assets/text/Text.hx
@@ -2,4 +2,12 @@ package contrast.object.assets.text;
 
 import flixel.text.FlxText;
 
-class Text extends FlxText {}
+class Text extends FlxText
+{
+	override public function new(x = 0.0, y = 0.0, fieldWidth = 0.0, ?text:String, size = 18, embeddedFont = true)
+	{
+		super(x, y, fieldWidth, text, size, embeddedFont);
+
+        font = 'font:DOSAPP.ttf';
+	}
+}
