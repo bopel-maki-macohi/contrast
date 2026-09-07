e6a903e25b9a5a427b907e016a684b0fb0ce1209
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-07 15:50:49 -0500

    cleanup

--- source/contrast/object/assets/text/Text.hx
+++ source/contrast/object/assets/text/Text.hx
@@ -7,7 +7,6 @@ class Text extends FlxText
 	override public function new(x = 0.0, y = 0.0, fieldWidth = 0.0, ?text:String, size = 18, embeddedFont = true)
 	{
 		super(x, y, fieldWidth, text, size, embeddedFont);
-
-        font = 'font:DOSAPP.ttf';
+		font = 'font:DOSAPP.ttf';
 	}
 }
