bd8414acec6d97b718873ead5cdc8e73091173f7
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-07 11:11:25 -0500

    no

--- source/state/StateFirstChoice.hx
+++ source/state/StateFirstChoice.hx
@@ -1,6 +1,5 @@
 package state;
 
-import text.TextGlow;
 import flixel.FlxG;
 import sprite.*;
 
@@ -10,8 +9,6 @@ class StateFirstChoice extends State
 
 	private var blue:Sprite;
 	private var yellow:Sprite;
-
-	private var instruction:TextGlow;
 
 	private var arrow:Sprite;
 
@@ -26,11 +23,6 @@ class StateFirstChoice extends State
 		add(blue = new Sprite().loadBitmapCacheGraphic('blue_vessel').scaleTo(4));
 		add(yellow = new Sprite().loadBitmapCacheGraphic('yellow_vessel').scaleTo(4));
 		add(arrow = new Sprite().loadBitmapCacheGraphic('arrow').scaleTo(4));
-
-		add(instruction = new TextGlow(0,0,0,'Select your initial path', 16));
-
-		instruction.screenCenter();
-		instruction.y -= instruction.height * 10;
 
 		blue.screenCenter();
 		yellow.screenCenter();
