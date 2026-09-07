7e201fd5614437659767e0d34f2c67c05271dd4f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-07 10:40:55 -0500

    unused

--- source/tool/GeneralTool.hx
+++ source/tool/GeneralTool.hx
@@ -2,13 +2,6 @@ package tool;
 
 class GeneralTool
 {
-	public static function multiply(value:Null<Float>, ?amount:Null<Int>)
-	{
-		if (amount == null || value == null) return value;
-
-		return value * amount;
-	}
-
 	public static function repeat(method:Int->Void, ?amount:Null<Int>)
 	{
 		if (amount == null || method == null) return;
