544f9466e971f9ab88b78cef3725a47dd4299b3a
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-07 15:53:43 -0500

    had to change it cause the converter fucked it up

--- source/contrast/object/assets/text/Text.hx
+++ source/contrast/object/assets/text/Text.hx
@@ -4,9 +4,9 @@ import flixel.text.FlxText;
 
 class Text extends FlxText
 {
-	override public function new(x = 0.0, y = 0.0, fieldWidth = 0.0, ?text:String, size = 18, embeddedFont = true)
+	override public function new(x = 0.0, y = 0.0, fieldWidth = 0.0, ?text:String, size = 16, embeddedFont = true)
 	{
 		super(x, y, fieldWidth, text, size, embeddedFont);
-		font = 'font:DOSAPP.ttf';
+		font = 'font:MX437_CL_EAGLEII_8X16.ttf';
 	}
 }
