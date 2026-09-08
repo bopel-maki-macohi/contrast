47720390a887934ad419b874e53352b73dfc65af
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-07 20:30:50 -0500

    object typedef

--- source/contrast/state/StateOptions.hx
+++ source/contrast/state/StateOptions.hx
@@ -3,11 +3,26 @@ package contrast.state;
 import flixel.math.FlxPoint;
 import flixel.FlxG;
 
+typedef Option =
+{
+	id:String,
+	getLabel:Bool->Void,
+	onSelection:Void->Void,
+}
+
 class StateOptions extends State
 {
 	private var isBlue(default, null):Bool;
 
 	private var seaBG(default, null):SeaBackdrop;
+
+	private var options:Array<Option> = [
+		// {
+		// 	id: '',
+		// 	getLabel: null,
+		// 	onSelection: null,
+		// }
+	];
 
 	override public function new(?forceBlue:Null<Bool>)
 	{
@@ -22,6 +37,27 @@ class StateOptions extends State
 		super.create();
 
 		add(seaBG = new SeaBackdrop((isBlue) ? Color.BLUE : Color.YELLOW, FlxPoint.weak((isBlue) ? 100 : 0, (!isBlue) ? 2 : 0)));
+
+		if (options == null || options.length < 1)
+		{
+			var noOptionsText = new Text(0, 0, 0, 'NO OPTIONS PRESENT', 32);
+			add(noOptionsText);
+
+			noOptionsText.screenCenter();
+
+			#if !debug
+			leave();
+			#end
+
+			return;
+		}
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.ESCAPE) leave();
 	}
 
 	private function leave()
