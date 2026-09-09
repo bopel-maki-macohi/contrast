11926a8898c989432237cbcdc28ecff7e2a46334
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-09 07:21:56 -0500

    scale down user

--- source/contrast/state/blue/BlueStart.hx
+++ source/contrast/state/blue/BlueStart.hx
@@ -8,6 +8,9 @@ class BlueStart extends StateMovingUser
 	{
 		super.create();
 
+        user.scaleTo(2);
+        user.screenCenter();
+
 		add(point1 = new SpriteTargetPoint('white', user));
 		point1.screenCenter();
 		point1.onSelection.add(function()
