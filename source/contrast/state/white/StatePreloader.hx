8e9b160731d6cb5da727bd93cd8b5176828bb1f4
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-09-08 19:18:04 -0500

    s

--- source/contrast/state/white/StatePreloader.hx
+++ source/contrast/state/white/StatePreloader.hx
@@ -143,7 +143,7 @@ class StatePreloader extends State
 			FlxTween.tween(seaBG.colorBG, {alpha: 0.25}, 2, {ease: FlxEase.quintOut});
 
 			final easterEggTime = Macro.getDefined('EASTER_EGG_AINSTANT') ? 1 : 10 + Save.data.contrast;
-			trace('Easter Egg Wait Time: $easterEggTime');
+			trace('Easter Egg Wait Time: ${easterEggTime}s');
 			FlxTimer.wait(easterEggTime, potentialEasterEgg);
 		}
 	}
